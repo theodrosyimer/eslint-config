229b786b3c99aadbfaf4fd3df058a6ce1c115a40
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-26 10:20:30 +0100

    chore: update devDependencies in package.json and pnpm-lock.yaml

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  trailingComma: 'all',
+  semi: false,
+  singleQuote: true,
+}
+
+export default config
