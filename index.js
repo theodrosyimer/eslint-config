2d4adb5601ee097183bc325110b6073e384c7db5
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-04-12 00:09:28 +0200

    feat: export turboAddon from turbo.js in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { default as defaultConfig } from './default.js'
 export { default as reactConfig } from './react.js'
+export { turboAddon } from './turbo.js'
 
 export { default } from './default.js'
